078571e95b10011b5d0256c35b057e230d43c7ba
Laolu <peterso@purdue.edu>
2014-02-08 17:30:46 -0500

    adding the answer

---
+++ PA03/answer03.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<malloc.h>
+
+
+
+char * strcat_ex(char * * dest, int * n, const char * src)
+{
+size_t src_len = strlen(src);
+size_t dest_len = *dest == NULL ? 0 : strlen(*dest);
+if (*dest == NULL || src_len + dest_len + 1 >= *n)
+{
+        *n = 1 + 2 * (src_len + dest_len);
+        char *buffer = malloc(*n * sizeof(char));
+        *buffer = '\0';
+	if(*dest != NULL) strcpy(buffer, *dest);
+	free(*dest);
+	*dest = buffer;
+
+}
+	strcat(*dest, src);
+	return *dest;
+}
+
+char * * explode(const char * str, const char * delims, int * arrLen);
+{
+
+
+
+
+
+}
