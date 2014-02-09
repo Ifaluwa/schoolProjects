8e37742bb127f5952f4125602eeab719dcf5c4d7
Laolu <peterso@purdue.edu>
2014-02-08 19:30:04 -0500

    update

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -3,12 +3,10 @@
 #include<string.h>
 #include<malloc.h>
 
-
-
 char * strcat_ex(char * * dest, int * n, const char * src)
 {
-size_t src_len = strlen(src);
-size_t dest_len = *dest == NULL ? 0 : strlen(*dest);
+	size_t src_len = strlen(src);
+	size_t dest_len = *dest == NULL ? 0 : strlen(*dest);
 if (*dest == NULL || src_len + dest_len + 1 >= *n)
 {
         *n = 1 + 2 * (src_len + dest_len);
@@ -23,11 +21,43 @@ if (*dest == NULL || src_len + dest_len + 1 >= *n)
 	return *dest;
 }
 
-char * * explode(const char * str, const char * delims, int * arrLen);
+char * * explode(const char * str, const char * delims, int * arrLen)
 {
-does it work
-
-
-
-
+	*arrLen = 1;
+	const char *pos = str;
+	while(*pos !='\0')
+{
+	if(strchr(delims, *pos)) (*arrLen)++;
+	pos++;
 }
+
+	char **string_list = malloc(*arrLen * sizeof(char *));
+	int index = 0;
+	const char * start = str;
+	pos = str;
+	while(*pos != '\0')
+{
+	if(strchr(delims, *pos))
+	{
+	int len = pos - start;
+	string_list[index] = malloc((len + 1) * sizeof(char));
+	memcpy(string_list[index], start, len * sizeof(char));
+	string_list[index][len] = '\0';
+	index++;
+	start = pos + 1;
+	}
+	pos++;
+}
+	int len = pos - start;
+	string_list[index] = malloc((len + 1) * sizeof(char));
+	memcpy(string_list[index], start, len * sizeof(char));
+	string_list[index][len] = '\0';
+
+	return string_list;
+}
+char * implode(char * * strArr, int len, const char * glue)
+{
+
+
+
+
